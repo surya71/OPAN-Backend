829f9fc33bde75093d9048ea56b4b966f857df15
jitiendran <jitiendranjiji2000@gmail.com>
2021-03-26 07:47:55 +0530

    updated

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { Users } = require("../models/index");
-const {JWTsign} = require('../packages/auth/tokenize');
+const { JWTsign } = require("../packages/auth/tokenize");
 
 mongoose.connect(
   "mongodb+srv://admin:123@cluster0.o42dd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -24,43 +24,43 @@ db.once("open", function () {
 //     console.log("MongoDB connected");
 //   }
 // });
-class Student{
-    static async Login(req,res){
-      try {
-        const body = req.body;
-          Users.findOne({ Rid: body.Rid }, (err, user) => {
-          if (err) {
-            console.log(err);
-          }
-          // console.log(consignee);
-          if (!user) {
-            res.status(401).send("Invalid user Id");
-          } else if (body.Password !== user.Password) {
-            res.status(401).send("Invalid Password");
-          } else {
-            const token = JWTsign(user._id);
-            res.send({ token ,Designation:user.Designation});
-          }
-        });
-      } catch (error) {
-        console.log(error);
-      }
+class Student {
+  static async Login(req, res) {
+    try {
+      const body = req.body;
+      Users.findOne({ Rid: body.Rid }, (err, user) => {
+        if (err) {
+          console.log(err);
+        }
+        // console.log(consignee);
+        if (!user) {
+          res.status(401).send("Invalid user Id");
+        } else if (body.Password !== user.Password) {
+          res.status(401).send("Invalid Password");
+        } else {
+          const token = JWTsign(user._id);
+          res.send({ token, UserId: user.Rid, Designation: user.Designation });
+        }
+      });
+    } catch (error) {
+      console.log(error);
     }
-    static async Register(req,res){
-        try {
-            const user = new Users(req.body);
-            user.save((error, user) => {
-              if (error) {
-                return res.status(500).send("Invalid");
-              } else {
-                const token = JWTsign(user._id);
-                res.send({ token ,Designation:user.Designation});
-              }
-            });
-          } catch (error) {
-            console.log(error);
-          }
+  }
+  static async Register(req, res) {
+    try {
+      const user = new Users(req.body);
+      user.save((error, user) => {
+        if (error) {
+          return res.status(500).send("Invalid");
+        } else {
+          const token = JWTsign(user._id);
+          res.send({ token, Designation: user.Designation });
+        }
+      });
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
 module.exports = Student;
